b93c1ae76067cd1622d10a2ff5169629dbd8e037
Roman Grudzinski <grudzinski@gmail.com>
2015-05-21 22:45:33 +0300

    Finish refactoring

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = require('./lib/DataUpdater.js');
+var debug = require('debug');
+
+var DataUpdater = require('./lib/DataUpdater.js');
+var FileDriver = require('./lib/FileDriver.js');
+var UrlDriver = require('./lib/UrlDriver.js');
+
+module.exports = {
+
+	_debug: debug('data-updater'),
+
+	fromFile: function(file, interval) {
+		this._debug('fromFile');
+		return new DataUpdater(FileDriver, file, interval);
+	},
+
+	fromUrl: function(url, interval) {
+		this._debug('fromUrl');
+		return new DataUpdater(UrlDriver, url, interval);
+	}
+
+};
